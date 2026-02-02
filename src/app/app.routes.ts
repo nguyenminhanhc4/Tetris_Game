3b785858598b92b7a322a552daf1da6ec8cedc8f
nguyen minh anh <nanh2832003@gmail.com>
2026-02-02 16:57:44 +0700

    update app default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GameContainerComponent } from './features/game/components/game-container/game-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameContainerComponent },
+];
